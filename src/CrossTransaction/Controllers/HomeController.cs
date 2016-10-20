5b7a0a80af6382fdaa1a04ddf82003cbbb57d13d
richard <lynhu@163.com>
2016-10-20 20:37:39 +0800

    Update code.

--- src/CrossTransaction/Controllers/HomeController.cs
+++ src/CrossTransaction/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+//using com.okcoin.rest.future;
+//using com.okcoin.rest.stock;
+//namespace com.okcoin.rest
 
 namespace CrossTransaction.Controllers
 {
@@ -17,6 +20,14 @@ namespace CrossTransaction.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
+            return View();
+        }
+
+        public IActionResult Exchange()
+        {
+
+            //StockRestApi getRequest1 = new StockRestApi(url_prex);
+            //StockRestApi postRequest1 = new StockRestApi(url_prex, api_key, secret_key);
             return View();
         }
 
